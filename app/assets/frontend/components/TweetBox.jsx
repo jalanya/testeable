3b4ffe0c65c65fea33e9e51653490188b2d530d2
Jose Alanya <jose.alanya@gmail.com>
2016-05-03 00:13:06 -0500

    Tweets List

--- app/assets/frontend/components/TweetBox.jsx
+++ app/assets/frontend/components/TweetBox.jsx
@@ -1,6 +1,7 @@
 export default class TweetBox extends React.Component {
   render() {
     return (
+     <div className="row">
        <form>
          <div className="input-field">
            <textarea className="materialize-textarea" />
@@ -8,6 +9,7 @@ export default class TweetBox extends React.Component {
            <button className="btn right">Tweet</button>
          </div>
        </form>
+      </div>
     )
   }
  }
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,10 +1,18 @@
 import TweetBox from './components/TweetBox'
+import TweetsList from './components/TweetsList'
+
+let mockTweets = [
+  { id: 1, name: 'Samer Buna', body: 'My #FirstTweet' },
+  { id: 2, name: 'Samer Buna', body: 'My #SecondTweet' },
+  { id: 3, name: 'Samer Buna', body: 'My #ThirdTweet' }
+];
 
 class Main extends React.Component {
   render() {
     return (
      <div className="container">
        <TweetBox />
+       <TweetsList tweets={mockTweets}/>
      </div>
     );
   }
